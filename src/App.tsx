48d7ddb7d677b82320631f0a6edbf70677a7b7a0
BrandonSev <seveste.brandon@gmail.com>
2022-09-17 17:54:01 +0200

    add tailwind + initialize react router + header and button action on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,43 @@
+const Home: React.FC = () => {
+  return (
+    <div className="container px-4 mx-auto relative">
+      <div className="flex items-stretch justify-between mb-6">
+        <p className="py-2 px-4 inline-block text-md bg-indigo-300 rounded  text-black cursor-pointer">
+          Liste des wilders
+        </p>
+        <div className="flex gap-2">
+          <button className="text-black px-4 py-2 bg-indigo-300 text-sm rounded flex items-center gap-2">
+            Ajouter un wilder
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+          </button>
+          <button className="text-black px-4 py-2 bg-indigo-300 text-sm rounded flex items-center gap-2">
+            Ajouter des skills
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
